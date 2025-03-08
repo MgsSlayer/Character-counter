4bef41be4221315f1c0a8740dfe6cb377e3b2f48
Michael Oboho <michael.oboho@gmail.com>
2025-03-08 23:33:41 +0100

    Logic Updates 2

--- character-counter/src/App.jsx
+++ character-counter/src/App.jsx
@@ -1,21 +1,26 @@
+import React, {useState} from 'react';
 import Cards from './Cards.jsx';
-import Input from './Input.jsx';
+import Input from './input.jsx';
 import Header from './Header.jsx';
 import Hero from './Hero.jsx';
 import Density from './Density.jsx';
 
 
-function App({text, ignoreSpaes}) {
+function App() {
+    const [text, setText] = useState("");
+    const [ignoreSpaes, setIgnoreSpaces] = useState(false);
  
     const charCount = ignoreSpaes ? text.replace(/\s/g, "").length : text.length;
     const words = text.trim().split(/\s+/).filter(word => word.length > 0).length;
     const sentences = text.split(/[.!?]+/).filter(sentence => sentence.trim().length > 0).length;
 
+   
+
  return(
   <>
   <Header/>
   <Hero/>
-  <Input/>
+  <Input text={text} setText={setText}/>
   <div className='card-div'>
   <Cards colorClass="purple"count={charCount} value="Total Characters"/>
   <Cards colorClass="yellow" count={words} value="Word Count"/>
