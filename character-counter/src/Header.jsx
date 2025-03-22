5359223a171cd8a52df8223b600128e26a71f285
Michael Oboho <michael.oboho@gmail.com>
2025-03-22 06:20:23 +0100

    Minor visual updates6

--- character-counter/src/Header.jsx
+++ character-counter/src/Header.jsx
@@ -2,7 +2,9 @@ function Header(){
     return(
         <header className="header">
             <h3>Character Counter</h3>
+            <div className="tog-back">
             <i class="fa-solid fa-sun"></i>
+            </div>
         </header>
     )
 }
