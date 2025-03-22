7aed91f0e162d6115fa4267ef766dc0ab77f6e21
Michael Oboho <michael.oboho@gmail.com>
2025-03-22 04:57:59 +0100

    Minor visual updates4

--- character-counter/src/input.jsx
+++ character-counter/src/input.jsx
@@ -3,17 +3,19 @@ return(
     <>
     <div className="input-section">
         <textarea></textarea>
-        <div>
-        <label>   
-        <input type="checkbox"></input>
-        Exclude Spaces
-        </label>
-        <label>   
-        <input type="checkbox"></input>
-        Set Character Limit
-        </label>
+        <div className="extra">
+            <div className="settings">
+                <label>   
+                <input type="checkbox"></input>
+                Exclude Spaces
+                </label>
+                <label>   
+                <input type="checkbox"></input>
+                Set Character Limit
+                </label>
+            </div>
+            <p>Approx. reading time: 1min</p>
         </div>
-        <p></p>
     </div>
     </>
 )
